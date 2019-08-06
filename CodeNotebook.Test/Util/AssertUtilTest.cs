0589a015782750fa262e7831b37f667ca925161c
xingChen <w.xingchen@outlook.com>
2019-08-06 23:00:32 +0800

    完善NotNull函数

---
+++ CodeNotebook.Test/Util/AssertUtilTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CodeNotebook.Util;
+using Xunit;
+
+namespace Util
+{
+    public class AssertUtilTest
+    {
+        [Fact]
+        public void NotNull()
+        {
+            Assert.Throws<Exception>(() =>
+            {
+                AssertUtil.NotNull<object>(null, string.Empty);
+            });
+
+            Assert.Throws<Exception>(() =>
+            {
+                AssertUtil.NotNull(string.Empty, string.Empty);
+            });
+
+            Assert.Throws<Exception>(() =>
+            {
+                AssertUtil.NotNull("", string.Empty);
+            });
+
+            Assert.Throws<Exception>(() =>
+            {
+                AssertUtil.NotNull(" ", string.Empty);
+            });
+
+            Assert.Throws<Exception>(() =>
+            {
+                AssertUtil.NotNull(new string[0], string.Empty);
+            });
+
+            Assert.Throws<Exception>(() =>
+            {
+                AssertUtil.NotNull(new List<string>(), string.Empty);
+            });
+
+            Assert.Throws<Exception>(() =>
+            {
+                AssertUtil.NotNull(new Dictionary<string, object>(), string.Empty);
+            });
+
+            AssertUtil.NotNull("1", string.Empty);
+            AssertUtil.NotNull(new string[1] { "1" }, string.Empty);
+            AssertUtil.NotNull(new List<string> { "1" }, string.Empty);
+            AssertUtil.NotNull(new Dictionary<string, object> { { "1", "1" } }, string.Empty);
+        }
+    }
+}
--- CodeNotebook/Util/AssertUtil.cs
+++ CodeNotebook/Util/AssertUtil.cs
@@ -9,26 +9,96 @@ namespace CodeNotebook.Util
     /// </summary>
     public static class AssertUtil
     {
+        #region IsTrue
         /// <summary>
         /// 判断指定条件是否为true，如果该条件为false，则引发一个异常
         /// </summary>
         /// <exception cref="Exception">条件为false时抛出</exception>
         /// <param name="condition">表达式</param>
         /// <param name="message">错误信息</param>
-        public static void IsTrue(bool condition, string message)
+        public static void IsTrue(bool condition, string message = null)
         {
-            if (!condition) throw new Exception(message);
+            if (!condition) ThrowException(message);
         }
 
         /// <summary>
-        /// 判断指定对象是否非null，如果为null，则引发一个异常
+        /// 判断指定条件是否为true，如果该条件为false，则引发指定异常
         /// </summary>
-        /// <exception cref="Exception">条件为false时抛出</exception>
-        /// <param name="obj">预期非null的对象</param>
-        /// <param name="message">错误信息</param>
-        public static void IsNotNull(object obj, string message)
+        /// <param name="condition">表达式</param>
+        /// <param name="exception">条件为false时引发的异常</param>
+        public static void IsTrue(bool condition, Exception exception)
         {
-            if (obj == null) throw new Exception(message);
+            if (!condition) throw exception;
         }
+        #endregion
+
+        #region NotNull
+        /// <summary>
+        /// 判断指定对象是否非null，如果为null则引发一个异常
+        /// </summary>
+        /// <exception cref="Exception">对象为null时抛出</exception>
+        /// <param name="target">目标对象</param>
+        /// <param name="message">错误信息</param>
+        public static void NotNull(object target, string message = null)
+        {
+            if (target == null) ThrowException(message);
+        }
+
+        /// <summary>
+        /// 判断指定对象是否非null，如果为null则引发一个异常
+        /// </summary>
+        /// <param name="target">目标对象</param>
+        /// <param name="exception">目标对象为null时引发的异常</param>
+        public static void NotNull(object target, Exception exception)
+        {
+            if (target == null) throw exception;
+        }
+
+        /// <summary>
+        /// 判断指定的字符串是否为null、空或一连串的空白字符，如果为null、空或一连串的空白字则引发一个异常
+        /// </summary>
+        /// <exception cref="Exception">指定字符串为null、空或一连串的空白字时抛出</exception>
+        /// <param name="target">目标对象</param>
+        /// <param name="message">错误信息</param>
+        public static void NotNull(string target, string message = null)
+        {
+            if (string.IsNullOrWhiteSpace(target.ToString())) ThrowException(message);
+        }
+
+        /// <summary>
+        /// 判断指定的字符串是否为null、空或一连串的空白字符，如果为null、空或一连串的空白字则引发一个异常
+        /// </summary>
+        /// <exception cref="Exception">指定字符串为null、空或一连串的空白字时抛出</exception>
+        /// <param name="target">目标对象</param>
+        /// <param name="exception">指定字符串为null、空或一连串的空白字时引发的异常</param>
+        public static void NotNull(string target, Exception exception)
+        {
+            if (string.IsNullOrWhiteSpace(target.ToString())) throw exception;
+        }
+
+        /// <summary>
+        /// 判断指定对象是否非null，如果为null或长度为0，则引发一个异常
+        /// </summary>
+        /// <exception cref="Exception">对象为null或长度为0时抛出</exception>
+        /// <param name="target">目标对象</param>
+        /// <param name="message">错误信息</param>
+        public static void NotNull<T>(IEnumerable<T> target, string message = null)
+        {
+            if (target == null || !target.GetEnumerator().MoveNext()) ThrowException(message);
+        }
+
+        /// <summary>
+        /// 判断指定对象是否非null，如果为null或长度为0，则引发一个异常
+        /// </summary>
+        /// <exception cref="Exception">对象为null或长度为0时抛出</exception>
+        /// <param name="target">目标对象</param>
+        /// <param name="exception">目标对象为null或长度为0时引发的异常</param>
+        public static void NotNull<T>(IEnumerable<T> target, Exception exception)
+        {
+            if (target == null || !target.GetEnumerator().MoveNext()) throw exception;
+        }
+        #endregion
+
+        private static void ThrowException(string message) => throw new Exception(message);
     }
 }
