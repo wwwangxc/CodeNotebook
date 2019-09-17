6b62fa8e03688ed1b00da4fab383139700891775
xingChen <w.xingchen@outlook.com>
2019-09-17 15:42:57 +0800

    增加MD5加密

---
+++ CodeNotebook/Util/CipherUtil.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CodeNotebook.Util.Handler;
+
+namespace CodeNotebook.Util
+{
+    public class CipherUtil
+    {
+        private volatile static Dictionary<string, ICipherHandler> _handler = new Dictionary<string, ICipherHandler>();
+        private readonly static object _sysLock = new object();
+        private CipherUtil() { }
+        private static ICipherHandler GetHandler<T>() where T : ICipherHandler, new()
+        {
+            ICipherHandler handler;
+            var fullName = typeof(T).FullName;
+
+            if (!_handler.TryGetValue(fullName, out handler))
+            {
+                lock (_sysLock)
+                {
+                    if (!_handler.TryGetValue(fullName, out handler))
+                    {
+                        handler = Activator.CreateInstance<T>();
+                        _handler.Add(fullName, handler);
+                    }
+                }
+            }
+
+            return handler;
+        }
+
+        public ICipherHandler MD5 => GetHandler<Md5Handler>();
+
+    }
+
+    public interface ICipherHandler
+    {
+        /// <summary>
+        /// 加密
+        /// </summary>
+        /// <param name="arg"></param>
+        /// <returns></returns>
+        string Encrypt(string arg);
+        /// <summary>
+        /// 解密
+        /// </summary>
+        /// <param name="arg"></param>
+        /// <returns></returns>
+        string Decrypt(string arg);
+    }
+}
---
+++ CodeNotebook/Util/Handler/Md5Handler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeNotebook.Util.Handler
+{
+    public class Md5Handler : ICipherHandler
+    {
+        public string Decrypt(string arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Encrypt(string arg)
+        {
+            using (var md5 = System.Security.Cryptography.MD5.Create())
+            {
+                var str = md5.ComputeHash(Encoding.UTF8.GetBytes(arg + SecretKey));
+                var returnStr = BitConverter.ToString(
+                    md5.ComputeHash(Encoding.UTF8.GetBytes(BitConverter.ToString(str).Replace("-", "0") + SecretKey)))
+                    .Replace("-", string.Empty).ToLower();
+                return returnStr;
+            }
+        }
+
+        private const string SecretKey = "f2eb43bd8d5b42af95ce207cecb5934f";
+    }
+}
